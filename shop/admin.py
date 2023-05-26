e6c733d856ac7e5c47d14238bd14ba31e8b567e4
Amir Motahari <amir.motahari@outlook.com>
2023-05-26 11:25:07 +0330

    add Car model to Django Admin

--- shop/admin.py
+++ shop/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.utils.translation import gettext_lazy as _
 from django.contrib.auth.admin import UserAdmin
 
-from shop.models import User
+from shop.models import User, Car
 
 class CustomAdmin(admin.AdminSite):
     site_header = _('سامانه فروش خودرو')
@@ -14,3 +14,8 @@ admin_site = CustomAdmin()
 @admin.register(User, site=admin_site)
 class UserAdmin(UserAdmin):
     search_fields = ('username', 'last_name')
+
+
+@admin.register(Car, site=admin_site)
+class CarAdmin(admin.ModelAdmin):
+    search_fields = ('id', )
